763421744821e60b493983c04b4e4d10b982ba96
Siyuan Zhou <visualzhou@gmail.com>
2011-10-10 20:59:50 -0400

    clean assignment 1 workspace

--- langmodel/LanguageModel.java
+++ langmodel/LanguageModel.java
@@ -6,6 +6,12 @@ import java.util.List;
  * Language models assign probabilities to sentences and generate sentences.
  */
 public interface LanguageModel {
-  double getSentenceProbability(List<String> sentence);
-  List<String> generateSentence();
+	public static final String UNKNOWN = "*UNKNOWN*";
+	public static final String UNSEEN = "*UNSEEN*";
+	public static final String STOP = "</S>";
+	public static final String START = "</S>";
+
+	double getSentenceProbability(List<String> sentence);
+
+	List<String> generateSentence();
 }
